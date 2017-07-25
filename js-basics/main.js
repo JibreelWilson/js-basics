e15e6d0aecf0e8b2bba0d60bbe87659adad77dce
J.B. Wilson <jibreelwilson14@gmail.com>
2017-07-24 20:08:26 -0600

    js-basics

---
+++ js-basics/main.js
@@ -0,0 +1,11 @@
+// Browser JS Basics
+
+// Requirements
+// -----------
+
+// - Create a blank, valid HTML5 document
+// - Create a javascript file with the content alert('Hello Boulder!'); and include it in your page with the script tag
+// - <em>Check Your Work</em>: When you refresh the page, you should see an alert box that interrupts you with the message, "Hello Boulder!"
+
+
+alert('Hello Boulder!')
---
+++ personal hello/main.js
@@ -0,0 +1,8 @@
+// Part II: Personal Hello
+// -----------
+// - Create a blank, valid HTML5 document
+// - Create a javascript file and include it with a script tag as before
+// - Use var, prompt, string literals, the + operator, and alert such that when the user opens the page, it will ask them for their name, and then respond with "Hello [YOUR_NAME]!"
+var YOUR_NAME=prompt('What is your name?')
+
+alert("Hello " + YOUR_NAME + "!")
